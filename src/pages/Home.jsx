362990f13a1c1c7bd51696055ff55754ed24357f
Sam Wilcox <sam@groundgigs.com>
2026-06-20 16:42:54 -0400

    change mobile image h-screen to h-dvh which should elimate the image moving downward when the bottom bar of the mobile browser goes away

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -29,11 +29,8 @@ const Home = () => {
   // const filterOpacity = 0.8 + scrollRatio * 0.2;
   // const gradientFade = scrollRatio; // used for crossfading gradients
   const scrollThreshold = 250;
-  const scrollThresholdMobile = 100;
   const isZoomed = scrollY > scrollThreshold;
-  const isZoomedMobile = scrollY > scrollThresholdMobile;
   const zoom = isZoomed ? maxZoom : 1;
-  const zoomMobile = isZoomedMobile ? maxZoom : 1;
   const filterOpacity = isZoomed ? 1 : 0.8;
   const gradientFade = isZoomed ? 1 : 0;
 
@@ -54,7 +51,7 @@ const Home = () => {
         alt='Background'
         className='block md:hidden fixed bottom-0 left-1/2 w-[100%] h-dvh object-cover -z-20 transition-transform duration-700 ease-out'
         style={{
-          transform: `translateX(-50%) scale(${zoomMobile})`,
+          transform: `translateX(-50%) scale(${zoom})`,
           transformOrigin: "center center",
         }}
       />
